50676d133017c2cbfc2babd5eb83505c5e09a026
Pascal Mouret <pascal.mouret@me.com>
2016-07-01 17:22:51 +0200

    exercise 3.2

---
+++ src/main/scala/chapter3/List.scala
@@ -0,0 +1,16 @@
+package chapter3
+
+trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  def tail[A](l: List[A]): List[A] = l match {
+    case Cons(_, tail) => tail
+    case _ => sys.error("empty list")
+  }
+}
