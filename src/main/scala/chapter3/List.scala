dc570411d92778bd9eaf7977a2e2eb7478843606
Pascal Mouret <pascal.mouret@me.com>
2016-07-01 17:35:43 +0200

    exercise 3.4

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -11,11 +11,22 @@ object List {
 
   def tail[A](l: List[A]): List[A] = l match {
     case Cons(_, t) => t
-    case _ => sys.error("empty list")
+    case Nil => sys.error("empty list")
   }
 
-  def setHead[A](h: A, l: List[A]): List[A] = l match {
+  def setHead[A](l: List[A], h: A): List[A] = l match {
     case Cons(_, t) => Cons(h, t)
-    case _ => sys.error("empty list")
+    case Nil => sys.error("empty list")
   }
+
+  @annotation.tailrec
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Cons(_, t) if n > 0 => drop(t, n - 1)
+    case c: Cons[A] => c
+    case Nil => sys.error("list too short")
+  }
+}
+
+object Main extends App {
+  println(List.drop(List(1,2,3,4,5,6,7,8,9,10), 5))
 }
