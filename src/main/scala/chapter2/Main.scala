430fec92845a844af4261a7390fcee2f8588a2f0
Pascal Mouret <pascal.mouret@me.com>
2016-07-01 15:10:01 +0200

    exercise 2.2

--- src/main/scala/chapter2/Main.scala
+++ src/main/scala/chapter2/Main.scala
@@ -3,13 +3,23 @@ package chapter2
 object Main extends App {
   def fib(n: Int): Int = {
     @annotation.tailrec
-    def go(oldFib: Int, newFib: Int, n: Int): Int = {
+    def go(n: Int, oldFib: Int, newFib: Int): Int = {
       if (n == 0) newFib
-      else go(newFib, oldFib + newFib, n - 1)
+      else go(n - 1, newFib, oldFib + newFib)
     }
 
-    go(0, 1, n - 2) // since we already start with the first two numbers
+    go(n - 2, 0, 1) // since we already start with the first two numbers
   }
 
-  println(fib(6))
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    def go(i: Int): Boolean = {
+      if (i == 1) ordered(as(i - 1), as(1))
+      else if (ordered(as(i - 1), as(i))) go(i - 1)
+      else false
+    }
+
+    go(as.length - 1)
+  }
+
+  println(isSorted[Int](Array(1, 2, 3), _ < _))
 }
