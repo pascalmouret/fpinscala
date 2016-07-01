71eb0a0008724d7b042d7c5c5dae8d3e9f2081d7
Pascal Mouret <pascal.mouret@me.com>
2016-07-01 15:22:36 +0200

    exercise 2.3

--- src/main/scala/chapter2/Main.scala
+++ src/main/scala/chapter2/Main.scala
@@ -12,6 +12,7 @@ object Main extends App {
   }
 
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
     def go(i: Int): Boolean = {
       if (i == 1) ordered(as(i - 1), as(1))
       else if (ordered(as(i - 1), as(i))) go(i - 1)
@@ -20,6 +21,9 @@ object Main extends App {
 
     go(as.length - 1)
   }
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
 
   println(isSorted[Int](Array(1, 2, 3), _ < _))
 }
